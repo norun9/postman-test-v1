8bb56dcd9180739741f2e93f36709429b24fef5b
norun9 <u.t012807@gmail.com>
2022-08-10 20:23:15 +0900

    push

--- backend/internal/api/usecase/output/posts.go
+++ backend/internal/api/usecase/output/posts.go
@@ -1,24 +1,17 @@
 package output
 
 import (
-	"time"
-
 	"github.com/norun9/postmantest/internal/api/domain/model"
-	"github.com/volatiletech/null/v8"
 )
 
 type ChildPost struct {
-	ID        int64       `json:"id"`
-	Content   null.String `json:"content"`
-	CreatedAt time.Time   `json:"created_at"`
-	UpdatedAt time.Time   `json:"updated_at"`
+	ID      int64  `json:"id"`
+	Content string `json:"content"`
 }
 
 func GetChildPost(post *model.Post) *ChildPost {
 	return &ChildPost{
-		ID:        post.ID,
-		Content:   post.Content,
-		CreatedAt: post.CreatedAt,
-		UpdatedAt: post.UpdatedAt,
+		ID:      post.ID,
+		Content: post.Content.String,
 	}
 }
